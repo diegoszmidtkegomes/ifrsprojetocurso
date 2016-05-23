68959db9045721a00d3746cef322681cde0e871a
Rhuan Barros <rhuanbarros@gmail.com>
2016-05-22 22:51:55 -0300

    arraylists no dao

--- Zava/src/dao/Dao.java
+++ Zava/src/dao/Dao.java
@@ -1,5 +1,15 @@
 package dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import models.*;
+
 public class Dao {
+	static public List<Disciplina> disciplinas = new ArrayList<>();
+	static public List<Estudante> estudantes = new ArrayList<>();
+	static public List<Matricula> matriculas = new ArrayList<>();
+	static public List<Professor> professores = new ArrayList<>();
+	static public List<Turma> turmas = new ArrayList<>();
 
 }
